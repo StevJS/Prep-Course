d3e3b427c8c894062cd2b2605976cd5739417725
Stiven Valeriano <jsvaleriano321@live.com>
2022-07-14 11:25:29 -0500

    Finalizado hasta JS04

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -90,23 +90,25 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
-  function ArrayAvg(myArray) {
-    var i = 0, summ = 0, ArrayLen = myArray.length;
-    while (i < ArrayLen) {
-        summ = summ + myArray[i++];
+  var sumaTotal = 0;
+  var numeroTest = resultadosTest.length;
+  for (var i = 0; i < numeroTest; i++) {
+    sumaTotal += resultadosTest[i]; 
+  }  
+  var promedio = sumaTotal  / resultadosTest.length; 
+  return promedio;
 }
-    return summ / ArrayLen;
-}
-var myArray = [1, 5, 2, 3, 7];
-var a = ArrayAvg(myArray);
-console.log(a)
-}
-
 
 function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+  var acumulador = 0;
+  for (let i = 0; i < numeros.length; i++) {
+    if ( numeros[i] > acumulador ) {  
+    acumulador = numeros[i];
+    }
+  } return acumulador;
 }
 
 
@@ -114,13 +116,26 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  var producto = 1;
+  if (arguments.length === 0) {
+    return 0;
+  } else {
+    for (let i = 0; i < arguments.length; i++) {
+      producto *= arguments[i];   
+    }
+  } return producto;
 }
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+  var cantidad = 0;
+  for (var i = 0; i< arreglo.length; i++) {
+    if (arreglo[i] > 18 ){
+      cantidad++;
+    }
+  } return cantidad;
 }
 
 
@@ -129,7 +144,9 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  
+  if (numeroDeDia === 1 || numeroDeDia === 7) 
+    return "Es fin de semana";
+    return "Es dia Laboral";
 } 
 
 
@@ -137,7 +154,10 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
+  var numero = n.toString();
+  var primeNum = numero.split("");
+  if( primeNum[0] === "9") return true;
+  else return false;
 }
 
 
@@ -145,7 +165,8 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  
+  let igual = arreglo.every((e) => e === arreglo[0]);
+  return igual;
 } 
 
 
@@ -154,6 +175,9 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  var nuevoArray = array.filter((e) => e === "Enero" || e === "Marzo" || e === "Noviembre");
+  if (nuevoArray.length < 3) return "No se encontraron los meses pedidos";
+  return nuevoArray;
 }
 
 
@@ -161,6 +185,12 @@ function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+  var nuevoArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > 100){
+     nuevoArray.push(array[i]);
+    } 
+  } return nuevoArray;
 }
 
 
@@ -172,6 +202,7 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  
 }
 
 
